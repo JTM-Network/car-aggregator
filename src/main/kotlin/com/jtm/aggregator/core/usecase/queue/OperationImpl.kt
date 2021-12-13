379125fdff1c35bd2bc99ee5fc858674cc7321d5
Joe Matthews <joetymatthews@gmail.com>
2021-12-13 14:49:05 +0000

    refactor: add logging for operation

--- src/main/kotlin/com/jtm/aggregator/core/usecase/queue/OperationImpl.kt
+++ src/main/kotlin/com/jtm/aggregator/core/usecase/queue/OperationImpl.kt
@@ -2,19 +2,23 @@ package com.jtm.aggregator.core.usecase.queue
 
 import com.jtm.aggregator.core.domain.model.OperationEvent
 import com.jtm.aggregator.data.manager.QueueManager
+import org.slf4j.LoggerFactory
 import java.util.*
 
-abstract class OperationImpl(val queueManager: QueueManager, val id: UUID = UUID.randomUUID(), val name: String, private val priority: Int): Operation {
+abstract class OperationImpl(private val queueManager: QueueManager, val id: UUID = UUID.randomUUID(), val name: String, private val priority: Int): Operation {
 
+    private val logger = LoggerFactory.getLogger(OperationImpl::class.java)
     private var startTime: Long = 0
     private var taken: Long = 0
 
     override fun init() {
         this.startTime = System.currentTimeMillis()
+        logger.info("Operation started: $startTime")
     }
 
     override fun post() {
         this.taken = (System.currentTimeMillis() - startTime)
+        logger.info("$name operation took: $taken ms")
     }
 
     override fun sendEvent(event: OperationEvent) {
--- src/main/kotlin/com/jtm/aggregator/data/worker/QueueWorker.kt
+++ src/main/kotlin/com/jtm/aggregator/data/worker/QueueWorker.kt
@@ -4,6 +4,7 @@ import com.jtm.aggregator.core.domain.entity.OperationStats
 import com.jtm.aggregator.core.usecase.repository.OperationStatsRepository
 import com.jtm.aggregator.data.manager.QueueManager
 import org.slf4j.LoggerFactory
+import kotlin.math.log
 
 class QueueWorker(private val queueManager: QueueManager, private val statsRepository: OperationStatsRepository): Runnable {
 
@@ -15,7 +16,13 @@ class QueueWorker(private val queueManager: QueueManager, private val statsRepos
     }
 
     fun process() {
-        val operation = queueManager.getOperation() ?: return
+        val operation = queueManager.getOperation()
+        if (operation == null) {
+            logger.info("Operation not found.")
+            return
+        }
+
+        logger.info("Operation found ${operation.name()}...")
         operation.init()
         operation.run().block()
         operation.post()
